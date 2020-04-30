9df59d572afdcdc66716feaabc498ca15de77e23
MeLuque <63817065+MeLuque@users.noreply.github.com>
2020-04-30 05:42:51 -0300

    Add files via upload

---
+++ 01-Adicion/adicion.cpp
@@ -0,0 +1,16 @@
+/*  Trabajo Práctico 1: Adición
+    Melissa Luque
+    27/04/2020
+*/
+#include <iostream>
+
+int main(){
+    int a, b, suma;
+    std::cout<<"Ingrese un numero entero:\n";
+    std::cin>> a;
+    std::cout<<"Ingrese otro numero entero:\n";
+    std::cin>> b;
+    suma = a + b;
+    std::cout<< "La suma es: "<<suma;
+
+}
